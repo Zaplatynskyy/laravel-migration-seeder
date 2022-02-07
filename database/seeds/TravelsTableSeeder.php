d43d3ddfe59dc7121dea3716b1d441f6ae9da2e7
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-07 18:26:51 +0100

    non funziona il lancio seeder

---
+++ database/seeds/TravelsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Travel;
+
+class TravelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0;$i < 10; $i++) {
+            $travel = new Travel();
+            $travel->name = $faker->firstName();
+            $travel->surname = $faker->lastName();
+            $travel->destination = $faker->state();
+            $travel->departure = $faker->date();
+            $travel->arrival = $faker->date();
+            $travel->price = $faker->randomFloat(2, 99, 999999);
+            $travel->text = $faker->paragraph();
+            $travel->overnight_stay = $faker->numberBetween(0, 1);
+            $travel->save();
+        }
+    }
+}
