5bae33d22d82ccef8c2caa6d21fbcd5e68731d0d
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-07 17:23:30 +0100

    migration table

---
+++ database/migrations/2022_02_07_155708_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('destination', 50);
+            $table->date('departure');
+            $table->date('arrival');
+            $table->float('price', 8, 2);
+            $table->text('description');  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
